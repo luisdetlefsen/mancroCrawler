60c7baf80ee15ec66742e6847578423da9cc8607
Luis Detlefsen <lgdetlef@gmail.com>
2018-09-16 20:07:54 -0600

    R charts

--- mancro.r
+++ mancro.r
@@ -1,4 +1,6 @@
 
+
+
 setwd("D:/mancro_archive")
 
 properties <- read.csv("mancro.csv")
@@ -9,3 +11,33 @@ str(properties$precioRenta)
 
 summary(properties$precioRenta)
 
+
+ggplot(properties, aes(properties$zona, properties$precioVenta)) + geom_point()
+
+
+
+
+
+
+library(plotly)
+
+xconf <- list(
+  autotick = FALSE,
+  ticks = "outside",
+  tick0 = 0,
+  dtick = 1,
+  ticklen = 5,
+  tickwidth = 2,
+  tickcolor = toRGB("blue"),
+  title = "Zona"
+)
+
+yconf <- list(
+  autotick = TRUE,
+  ticks = "outside",
+  ticklen = 5,
+  tickwidth = 2,
+  tickcolor = toRGB("blue"),
+  title = "Precio venta (USD)"
+)
+plot_ly(properties, x = ~zona, y = ~precioVenta , type="scatter", mode="markers" , marker=list(color="purple" , size=20 , opacity=0.5)  ) %>%layout(xaxis = xconf, yaxis = yconf)
